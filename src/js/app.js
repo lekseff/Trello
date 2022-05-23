a11b4c375c8bcf95169d2fc284bdfb8c6db9b69c
Aleksey Zayachnikov <lekseff@gmail.com>
2022-05-23 17:52:49 +0300

    merge

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
 /* eslint-disable no-unused-vars */
 // TODO: write code here
 
+const oldInput = document.querySelector('.input-file');
+const newInput = document.querySelector('.newInput');
+
+newInput.addEventListener('click', () => {
+  console.log('клик');
+});
